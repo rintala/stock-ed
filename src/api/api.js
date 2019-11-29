7dd9185f90a7cea2c8536411435e1745f4e7382c
Andreas Cedervall <cederv@kth.se>
2019-11-29 16:16:39 +0100

    now able to change interval on the chart

--- src/api/api.js
+++ src/api/api.js
@@ -5,6 +5,7 @@ function getURL(options) {
     const urls = {
         stockSearch: `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${options.query}&apikey=${envVar.alphaVantageApiKey}`,
         stockDetailsToday: `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&outputsize=${options.outputsize}&interval=${options.interval}min&symbol=${options.symbol}&apikey=${envVar.alphaVantageApiKey}`,
+        stockDetailsLastYear: `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${options.symbol}&outputsize=${options.outputsize}&apikey=${envVar.alphaVantageApiKey}`,
         stockQuote: `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${options.symbol}&apikey=${envVar.alphaVantageApiKey}`
     }
 
@@ -15,6 +16,7 @@ function getURL(options) {
 export function alphaVantageApiCall(options) {
     // Symbol search options = query
     // Detail search options = interval [1, 5, 15, 30, 60](min) and symbol (e.g. GOOL) outputsize = compact / full
+    console.log('this is from the api call', options)
     return fetch(getURL(options), {
         method: 'GET'
     })
